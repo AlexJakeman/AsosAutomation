c46b0c62e93d07c75fcf4f5d3e0632abc8cb2097
Alex Jakeman <alexjakemann4@gmail.com>
2023-01-04 15:58:19 +0000

    Updated it statement in test

--- Cypress/e2e/spec.cy.js
+++ Cypress/e2e/spec.cy.js
@@ -1,7 +1,8 @@
+import { it } from 'mocha';
 import NavigateAndAcceptCookies from '../Fixtures/example';
 
 describe('Visit Asos and assert product added to basket', () => {
-  it('navigates to asos homepage', () => {
+  it('navigates to asos, adds to basket and asserts that item is in basket', () => {
     const asos = new NavigateAndAcceptCookies();
     asos.navigateToAsos();
     asos.acceptCookies();
@@ -10,7 +11,7 @@ describe('Visit Asos and assert product added to basket', () => {
     asos.addToBasket();
     asos.assertItemInBasket();
   })
-})
+}),
 
 Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
